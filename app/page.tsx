5fbc0c9d4b3e9837c80038c21483b90fe62d8698
RoustyTousty <emils.misisons@gmail.com>
2024-11-08 14:57:27 +0200

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import LoadingScreen from "@/components/LoadingScreen";
 import OrderInputScreen from "@/components/OrderInputScreen";
+import Test from "@/components/Test";
 
 export default function Home() {
   return (
     <div className="h-screen">
+      <Test/>
       <OrderInputScreen/>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,36 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#253746',
-        secondary: '#66903a',
-        accent: '#f89915',
+  
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          "primary": "#253746",
+          "secondary": "#f89915",
+          "accent": "#66903a",
+          "neutral": "#ffff",
+          "base-100": "#253746",
+          "info": "#ffffff",
+          "success": "#66903a",
+          "warning": "#f89915", 
+          "error": "#ff0000",
+        },
+        light: {
+          "primary": "#ffffff",
+          "secondary": "#ffffff",
+          "accent": "#ffffff",
+          "neutral": "#ffffff",
+          "base-100": "#ffffff",
+          "info": "#ffffff",
+          "success": "#ffffff",
+          "warning": "#ffffff", 
+          "error": "#ff0000",
+        }
       },
-    },
+    ],
   },
+
   plugins: [
     require('daisyui'),
   ],
